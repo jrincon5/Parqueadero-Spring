f45c634626c6a9fafa3406371ad6d51de7c97940
Julian <jrincon5@eafit.edu.co>
2018-01-29 22:22:02 -0500

    first commit

---
+++ src/main/java/co/com/ceiba/parqueadero/controller/helloController.java
@@ -0,0 +1,69 @@
+package co.com.ceiba.parqueadero.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import co.com.ceiba.parqueadero.model.persona;
+
+@Controller
+@RequestMapping("say")
+public class helloController {
+
+	//Llamar una vista (REDIRECCION)
+	@GetMapping("/hello1")
+	public String helloWorld(Model model) {
+		model.addAttribute("name", "Así paso parametros 1");
+		return "hello";
+	}
+	
+	//Insertar datos en plantillas
+	@GetMapping("/hello2")
+	public ModelAndView exampleMav() {
+		return new ModelAndView("hello","name","Así paso parametros 2");
+	}
+	
+	//Insertar varios datos
+	@GetMapping("/persona1")
+	public String personaDatos(Model model) {
+		model.addAttribute("persona",new persona("Julian",22));
+		return "hello";
+	}
+	
+	//Insertar varios datos 2
+	@GetMapping("/persona2")
+	public ModelAndView personaDatosMav() {
+		ModelAndView mav=new ModelAndView("hello");
+		mav.addObject("persona",new persona("Sara",34));
+		return mav;
+	}
+	/*
+	//Insertar lista de datos
+	@GetMapping("/gente1")
+	public String genteDatos(Model model) {
+		model.addAttribute("gente",getPeople());
+		return "hello";
+	}
+		
+	//Insertar lista de datos 2
+	@GetMapping("/gente2")
+	public ModelAndView genteDatosMav() {
+		ModelAndView mav=new ModelAndView("hello");
+		mav.addObject("gente",getPeople());
+		return mav;
+	}
+	
+	private List<persona> getPeople(){
+		List<persona> people= new ArrayList<>();
+		people.add(new persona("Tom",35));
+		people.add(new persona("Martha",40));
+		people.add(new persona("Samantha",32));
+		people.add(new persona("Will",19));
+		return people;
+	}*/
+}
---
+++ src/main/java/co/com/ceiba/parqueadero/model/persona.java
@@ -0,0 +1,29 @@
+package co.com.ceiba.parqueadero.model;
+
+public class persona {
+	
+	private String nombre;
+	private int edad;
+	
+	public persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}	
+}
